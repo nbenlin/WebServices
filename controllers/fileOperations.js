c2339a9bd86e943ad537e65698e237e233c470fc
Nihat Benli <nihatbenli.nb@gmail.com>
2020-12-12 17:41:07 +0100

    added /parse enpoint

--- controllers/fileOperations.js
+++ controllers/fileOperations.js
@@ -1,5 +1,7 @@
 const CustomError = require('../helpers/error/customError');
 const asyncErrorHandlerWrapper = require('express-async-handler');
+const formidable = require('formidable');
+const fs = require('fs');
 
 const storeData = asyncErrorHandlerWrapper( async (req, res, next) => {
     let storeArr = [];
@@ -15,6 +17,36 @@ const storeData = asyncErrorHandlerWrapper( async (req, res, next) => {
         data: storeArr
     });
 });
+
+const parsing = asyncErrorHandlerWrapper( async (req, res) => {
+    const form = formidable( { multiples: true });
+    form.parse(req, (err, fields, files) => {
+        fs.readFile(files.file.path, function read(err, data) {
+            const values = data.toString().trim().split(";");
+            const result = {};
+            values.forEach((item) => {
+                const parsedItem = item.replace(":", ";");
+                const [key, value] = parsedItem.split(";");
+                const parsedValue = Number(value);
+                if (!Number.isNaN(parsedValue)) {
+                    result[key] = parsedValue;
+                } else {
+                    const subValues = value.split(",");
+                    result[key] = {};
+                    subValues.forEach((subItem) => {
+                        const [subKey, subValue] = subItem.split(":");
+                        result[key][subKey] = Number(subValue);
+                    });
+                }
+            });
+            return res.status(200)
+            .json({
+                result: result
+            });
+        });
+    });
+});
 module.exports = {
-    storeData
+    storeData,
+    parsing
 };
--- routers/fileOperations.js
+++ routers/fileOperations.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { storeData } = require('../controllers/fileOperations');
+const { storeData, parsing } = require('../controllers/fileOperations');
 
 const router = express.Router();
 
 router.post('/store', storeData);
+router.post('/parse', parsing);
 
 module.exports = router;
